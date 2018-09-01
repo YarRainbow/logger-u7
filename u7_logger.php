859853c672e2a38e8e60d19e56dbdeea1e20a6c7
hokoo <igorekio@mail.ru>
2018-09-01 20:03:45 +0300

    Update u7_logger.php

--- u7_logger.php
+++ u7_logger.php
@@ -6,88 +6,122 @@ Author: WPCraft
 Author URI: https://wpcraft.ru/
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Version: 1.2
+Version: 1.3
 */
 
-class Logger_U7
-{
+class Logger_U7{
 
-  function __construct()
-  {
-    add_action('admin_menu', function(){
-      add_management_page(
-        $page_title = 'Logger',
-        $menu_title = 'Logger',
-        $capability = 'manage_options',
-        $menu_slug = 'logger_u7',
-        $func = array($this, 'display_page')
-      );
+	function __construct(){
+		add_action('admin_menu', function(){
+			add_management_page(
+				$page_title = 'Logger',
+				$menu_title = 'Logger',
+				$capability = 'manage_options',
+				$menu_slug = 'logger',
+				$func = array($this, 'display_page')
+			);
+		});
 
-    });
+		add_filter( "plugin_action_links_" . plugin_basename( __FILE__ ), array($this, 'add_settings_link') );
+		add_action( 'wp_ajax_logger_clear_log', array($this, 'clear_log') );
+		add_action('logger', array($this, 'add'));
+	}
 
-    add_filter( "plugin_action_links_" . plugin_basename( __FILE__ ), array($this, 'add_settings_link') );
+	function display_page(){
 
+		echo '<h1>Log</h1>';
+		echo '<p>For adding data to log use hook: <br><pre>do_action("logger", $var);</pre></p>';
+		echo '<a class="button clear_log" href="">Clear Log</a><hr>';
 
-    add_action('logger_u7', array($this, 'add'));
-  }
+		$data = get_option('logger_u7');
+		if( ! is_array($data)){
+			echo '<p>No data in log</p>';
+			return;
+		}
 
-  function display_page(){
+		$data = array_reverse($data);
+	?>
+	<table border="1" width="100%" class="logger_table">
+		<tr>
+		  <th>Date</th>
+		  <th>Data</th>
+		</tr>
+		<?php 
+			$i = 0;
+			foreach($data as $item): 
+			$i++;
+		?>
+		  <tr class="data" id="loggerDataRow_<?php echo $i; ?>">
+			<td valign="top" width="100px" class="dblClickToScroll" title="Double click for scrolling to next row">
+			  <span><?php echo $item['timestamp']; ?></span>
+			</td>
+			<td>
+			  <pre><?php var_dump($item['data']) ?></pre>
+			</td>
+		  </tr>
+		<?php endforeach; ?>
+	</table>
 
-    echo '<h1>Log</h1>';
-    echo '<p>For adding data to log use hook: <br><pre>do_action("logger_u7", $var);</pre></p><hr>';
+	<style>
+		.dblClickToScroll{ cursor : pointer }
+	</style>
+	<script>
+		jQuery( '.clear_log' ).click( function( eventObject ){
+			eventObject.preventDefault();
+			jQuery.post( 
+				'<?php echo get_admin_url() ?>/admin-ajax.php',
+				{
+					action : 'logger_clear_log'
+				},
+				function( response ){
+					response = JSON.parse( response );
+					if ( response.done == true ){
+						jQuery( '.logger_table tr.data' ).remove();
+					}
+				}
+			);
+		} )
+		
+		jQuery( '.dblClickToScroll' ).dblclick( function(){
+			var nextRow = jQuery( this ).closest( 'tr.data' ).next( 'tr.data' );
+			if ( nextRow.length )
+			jQuery('body,html').animate({scrollTop: nextRow.offset().top - 60}, 600);
+		} )
 
-    $data = get_option('logger_u7');
-    if( ! is_array($data)){
-      echo '<p>No data in log</p>';
-      return;
-    }
+		jQuery
+	</script>
+<?php
+	}
 
-    $data = array_reverse($data);
-    ?>
-      <table border="1" width="100%">
-        <tr>
-          <th>Timestamp</th>
-          <th>Data</th>
-        </tr>
-        <?php foreach($data as $item): ?>
-          <tr>
-            <td valign="top" width="100px">
-              <span><?php echo $item['timestamp']; ?></span>
-            </td>
-            <td>
-              <pre><?php var_dump($item['data']) ?></pre>
-            </td>
-          </tr>
-        <?php endforeach; ?>
-      </table>
-    <?php
-  }
+	function add($data = ''){
+		$log = get_option('logger_u7');
+		if(empty($log)){
+			$log = array();
+		}
 
-  function add($data = ''){
-    $log = get_option('logger_u7');
-    if(empty($log)){
-      $log = array();
-    }
+		$log[] = array(
+			'timestamp' => date("Y-m-d H:i:s"),
+			'data' => $data,
+		);
 
-    $log[] = array(
-      'timestamp' => date("Y-m-d H:i:s"),
-      'data' => $data,
-    );
+		$log = array_slice($log, -99, 99);
 
-    $log = array_slice($log, -99, 99);
+		update_option('logger_u7', $log, false);
+	}
 
-    update_option('logger_u7', $log, false);
-  }
-
-  /**
-  * Add fast link in plugins list
-  */
-  function add_settings_link($links)
-  {
-    $settings_link = '<a href="tools.php?page=logger_u7">Logger</a>';
-    array_unshift( $links, $settings_link );
-    return $links;
-  }
+	/**
+	* Add fast link in plugins list
+	*/
+	function add_settings_link($links){
+		$settings_link = '<a href="tools.php?page=logger_u7">Logger</a>';
+		array_unshift( $links, $settings_link );
+		return $links;
+	}
+  
+	function clear_log(){
+		update_option('logger_u7', array(), false);
+		wp_die( json_encode( array( 'done' => true ) ) );
+	}
 }
 
 new Logger_U7;
